b5873cf0f701bb6b585d26827ebab4407009dc3e
Jeff Schaefer <blkhwks19@yahoo.com>
2020-10-16 14:50:54 -0500

    Updated contact list, added ability to add new contact

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # contacts-client-react
 To run: npm run serve
+
+# TODO
+Able to add new contacts successfully! Move dialog and add button out of nav, so the list can be refreshed after adding a new contact.
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -4,7 +4,14 @@ import {
   List,
   ListItem,
   ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Divider,
+  ListItemSecondaryAction,
+  IconButton,
 } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
 
 class ContactList extends Component {
   state = {
@@ -14,24 +21,44 @@ class ContactList extends Component {
   componentDidMount() {
     axios.get('http://localhost:3002/contacts')
       .then(res => {
-        this.setState({ contacts: res.data.contacts });
+        this.setState({ contacts: res.data });
       });
+  }
+
+  getInitials(c) {
+    return c.firstName.charAt(0).toUpperCase() + c.lastName.charAt(0).toUpperCase();
   }
 
   render() {
     return (
       <Fragment>
         <List>
-          { this.state.contacts.map(contact => {
-            const name = `${contact.firstName} ${contact.lastName}`;
-            return (
-
-              <ListItem>
-                <ListItemText primary={name} />
-              </ListItem>
-            )
-          }) }
+          { 
+            this.state.contacts.map(contact => {
+              const name = `${contact.firstName} ${contact.lastName}`;
+              return (
+                <Fragment key={contact.id}>
+                  <Divider />
+                  <ListItem>
+                    <ListItemAvatar color="primary">
+                      <Avatar>AA</Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={name} />
+                    <ListItemSecondaryAction>
+                      <IconButton>
+                        <EditIcon color="primary" />
+                      </IconButton> 
+                      <IconButton>
+                        <DeleteIcon color="secondary" />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                </Fragment>
+              )
+            }) 
+          }
         </List>
+        <Divider />
       </Fragment>
     )
   }
